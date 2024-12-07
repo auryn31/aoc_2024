8c3612fccb58a0f372562ac8ea370f27b9b1ca67
Auryn Engel <auryn31@gmx.de>
2024-12-07 12:06:18 +0100

    day 7 part 2

--- day_07/index.exs
+++ day_07/index.exs
@@ -1,14 +1,20 @@
 defmodule Day07 do
   def part1(content) do
-    lines =
-      String.split(content, "\n")
-      |> Enum.filter(&(&1 != ""))
-      |> Enum.map(&parseLine/1)
-      |> Enum.filter(&isPossible/1)
-      |> Enum.map(fn {x, _} -> x end)
-      |> Enum.sum()
+    String.split(content, "\n")
+    |> Enum.filter(&(&1 != ""))
+    |> Enum.map(&parseLine/1)
+    |> Enum.filter(&isPossible/1)
+    |> Enum.map(fn {x, _} -> x end)
+    |> Enum.sum()
+  end
 
-    # IO.inspect(lines)
+  def part2(content) do
+    String.split(content, "\n")
+    |> Enum.filter(&(&1 != ""))
+    |> Enum.map(&parseLine/1)
+    |> Enum.filter(&isPossible/1)
+    |> Enum.map(fn {x, _} -> x end)
+    |> Enum.sum()
   end
 
   defp expressions(numbers) do
@@ -22,7 +28,8 @@ defmodule Day07 do
   defp generate_expressions([head | tail], expression) do
     add = generate_expressions(tail, expression ++ [head, :+])
     multiply = generate_expressions(tail, expression ++ [head, :*])
-    add ++ multiply
+    orType = generate_expressions(tail, expression ++ [head, :or])
+    add ++ multiply ++ orType
   end
 
   defp isPossible({result, values}) do
@@ -38,8 +45,10 @@ defmodule Day07 do
       x, nil -> x
       :+, acc -> {:add, acc}
       :*, acc -> {:mult, acc}
+      :or, acc -> {:or, acc}
       num, {:add, acc} -> acc + num
       num, {:mult, acc} -> acc * num
+      num, {:or, acc} -> String.to_integer("#{acc}#{num}")
     end)
   end
 
@@ -55,16 +64,13 @@ defmodule Day07 do
 
     {result, values}
   end
-
-  def part2(content) do
-  end
 end
 
 # fileContent = File.read!("input.test.txt")
 fileContent = File.read!("input.txt")
 IO.puts("Part 1")
 result = Day07.part1(fileContent)
-IO.puts("Result: #{result} Expected: 5329")
+IO.puts("Result: #{result} Expected: 663613490587")
 IO.puts("Part 2")
 result = Day07.part2(fileContent)
-IO.puts("Result: #{result} Expected: 2162")
+IO.puts("Result: #{result} Expected: 110365987435001")
