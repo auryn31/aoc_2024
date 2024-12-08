3ddf4537ef936ed9db9d9b09c2578b317382c821
Auryn Engel <auryn31@gmx.de>
2024-12-08 12:11:36 +0100

    day 8 part 2

--- day_08/index.exs
+++ day_08/index.exs
@@ -1,4 +1,4 @@
-defmodule Day07 do
+defmodule Day08 do
   def part1(content) do
     matrix =
       String.split(content, "\n")
@@ -19,17 +19,71 @@ defmodule Day07 do
   end
 
   def part2(content) do
+    matrix =
+      String.split(content, "\n")
+      |> Enum.filter(&(&1 != ""))
+      |> Enum.map(&parseLine/1)
+
+    set = toSet(matrix)
+    width = Enum.count(Enum.at(matrix, 0))
+    height = Enum.count(matrix)
+
+    Enum.reduce(set, set, fn {elem, positions}, acc ->
+      current = Map.get(acc, "#", [])
+      toAdd = calculateAdjacent2(set, {elem, positions}, width, height)
+      toAddVal = Map.get(toAdd, "#", [])
+      Map.put(acc, "#", current ++ toAddVal)
+    end)
+    |> countAntinodes()
   end
 
   defp countAntinodes(set) do
     antinodes = Map.get(set, "#", [])
 
     Enum.uniq(antinodes)
-    |> IO.inspect()
     |> Enum.count()
   end
 
-  defp calculateAdjacent(set, {elem, positions}, width, height) do
+  defp calculateAllPointOnLine({x1, y1}, {x2, y2}, width, height) do
+    m = (y2 - y1) / (x2 - x1)
+
+    for x <- 0..(width - 1), y <- 0..(height - 1) do
+      if on_line?(x1, y1, m, x, y) do
+        {x, y}
+      else
+        nil
+      end
+    end
+    |> Enum.filter(& &1)
+  end
+
+  defp on_line?(x1, y1, m, x, y) do
+    y - y1 == m * (x - x1)
+  end
+
+  defp calculateAdjacent2(set, {_, positions}, width, height) do
+    Enum.reduce(positions, set, fn {x1, y1}, acc ->
+      current = Map.get(acc, "#", [])
+
+      toAdd =
+        Enum.reduce(positions, acc, fn {x2, y2}, acc_inner ->
+          if x1 != x2 or y1 != y2 do
+            toAdd =
+              calculateAllPointOnLine({x1, y1}, {x2, y2}, width, height)
+
+            current = Map.get(acc_inner, "#", [])
+            Map.put(acc_inner, "#", current ++ toAdd)
+          else
+            acc_inner
+          end
+        end)
+
+      toAddVal = Map.get(toAdd, "#", [])
+      Map.put(acc, "#", current ++ toAddVal)
+    end)
+  end
+
+  defp calculateAdjacent(set, {_, positions}, width, height) do
     Enum.reduce(positions, set, fn {x1, y1}, acc ->
       current = Map.get(acc, "#", [])
 
@@ -90,8 +144,8 @@ end
 # fileContent = File.read!("input.test.txt")
 fileContent = File.read!("input.txt")
 IO.puts("Part 1")
-result = Day07.part1(fileContent)
-IO.puts("Result: #{result} Expected: 14")
+result = Day08.part1(fileContent)
+IO.puts("Result: #{result} Expected: 396")
 IO.puts("Part 2")
-result = Day07.part2(fileContent)
-IO.puts("Result: #{result} Expected: -")
+result = Day08.part2(fileContent)
+IO.puts("Result: #{result} Expected: 1200")
