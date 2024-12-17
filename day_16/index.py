c1534f8550fcd6b8a831d4579b6f877011a054ea
Auryn Engel <auryn31@gmx.de>
2024-12-17 20:13:46 +0100

    day 16 part 2

--- day_16/index.py
+++ day_16/index.py
@@ -1,7 +1,10 @@
 import time
 
-from typing import List, Set, Tuple, Dict
+from typing import List, Set, Tuple, Dict, Self
 import os
+from collections import deque
+import heapq
+
 
 clear = lambda: os.system("clear")
 
@@ -17,67 +20,101 @@ def part2(content: str) -> int:
     return seat_count(matrix, start, end)
 
 
+class Node:
+    def __init__(
+        self,
+        position: Tuple[int, int],
+        cost: int,
+        parent: Set[Self] | None,
+        direction: Tuple[int, int],
+    ):
+        self.position = position
+        self.cost = cost
+        self.parent = parent
+        self.direction = direction
+
+    def __str__(self) -> str:
+        return f"Node: {self.position} - {self.cost} - {self.direction} - {self.parent}"
+
+
 def seat_count(
     matrix: List[List[str]], start: Tuple[int, int], end: Tuple[int, int]
 ) -> int:
-    return "Not found"
     directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
-    distances: Dict[Tuple[int, int], Tuple[int, List[List[Tuple[int, int]]]]] = {}
-    possible_points: List[
-        Tuple[Tuple[int, int], Tuple[int, int], int, List[List[Tuple[int, int]]]]
-    ] = [(start, (0, 1), 0, [[start]])]
-    current = start
-    while len(possible_points) > 0:
-        (current, direction, cost, histories) = possible_points.pop(0)
-        if distances.get(current) is None:
-            new_histories = []
-            for h in histories:
-                new_h = h.copy()
-                new_h.append(current)
-                new_histories.append(new_h)
-            distances[current] = (cost, new_histories)
+    distances: Dict[Tuple[int, int], Node] = {}
+    queue: deque[Node] = deque([Node(start, 0, None, (0, 1))])
+    priority_queue = []
+    heapq.heappush(priority_queue, (0, Node(start, 0, set(), (0, 1))))
+    while len(queue) > 0:
+        current_node = queue.popleft()
+        if distances.get(current_node.position) is None:
+            distances[current_node.position] = current_node
+        else:
+            existing_node = distances[current_node.position]
+            if existing_node.cost == current_node.cost:
+                existing_node.parent.update(current_node.parent)
+            if existing_node.cost > current_node.cost:
+                distances[current_node.position] = current_node
 
         for d in directions:
             cost_to_add = 1001
-            if d is direction:
+            if d is current_node.direction:
                 cost_to_add = 1
-            next_cost = cost + cost_to_add
-            next_pos = (current[0] + d[0], current[1] + d[1])
+            next_cost = current_node.cost + cost_to_add
+            next_pos = (
+                current_node.position[0] + d[0],
+                current_node.position[1] + d[1],
+            )
             if matrix[next_pos[0]][next_pos[1]] == "#":
                 continue
 
-            if distances.get(next_pos) is None:
-                new_histories = []
-                for h in histories:
-                    new_h = h.copy()
-                    new_h.append(current)
-                    new_histories.append(new_h)
-                possible_points.append((next_pos, d, next_cost, new_histories))
-                distances[next_pos] = (next_cost, new_histories)
-                continue
+            if next_pos not in distances or distances[next_pos].cost >= next_cost:
+                parent = {current_node}
+                nextNode = Node(next_pos, next_cost, parent, d)
+                queue.append(nextNode)
 
-            (cost_existing, prev_histories) = distances[next_pos]
-            if cost_existing >= next_cost:
-                new_histories = []
-                for h in histories + prev_histories:
-                    new_h = h.copy()
-                    new_h.append(current)
-                    if new_h not in new_histories:
-                        new_histories.append(new_h)
-                possible_points.append((next_pos, d, next_cost, new_histories))
-                distances[next_pos] = (next_cost, new_histories)
-    print(len(distances[end][1]))
-    # print(distances[end])
-    all_paths = distances[end][1]
-    final_paths = []
-    for path in all_paths:
-        print(path)
-        if path[0] == end:
-            final_paths.append(path)
+    print("Counting paths")
+    result = count_paths(matrix, distances[end])
+    return result
 
-    print(len(final_paths))
 
-    return distances[end][0]
+def count_paths(matrix: List[List[str]], node: Node) -> int:
+    points = set()
+    visited_nodes = set()
+    nodeQueue = deque([node])
+
+    while nodeQueue:
+        currentNode = nodeQueue.popleft()
+        if currentNode in visited_nodes:
+            continue
+        visited_nodes.add(currentNode)
+        points.add(currentNode.position)
+        if currentNode.parent:
+            nodeQueue.extend(currentNode.parent)
+    lines = []
+    for i in range(len(matrix)):
+        line = ""
+        for j in range(len(matrix[i])):
+            if (i, j) in points:
+                line += "O"
+            else:
+                line += matrix[i][j]
+        lines.append(line)
+    clear()
+    print("\n".join(lines))
+    return len(points)
+
+
+def print_path(matrix: List[List[str]], path: Set[Tuple[int, int]]):
+    clear()
+    for i in range(len(matrix)):
+        line = ""
+        for j in range(len(matrix[i])):
+            if (i, j) in path:
+                line += "O"
+            else:
+                line += matrix[i][j]
+        print(line)
 
 
 def search_finish(
@@ -91,7 +128,6 @@ def search_finish(
     current = start
     while len(possible_points) > 0:
         (current, direction, cost) = possible_points.pop(0)
-        # print(f"Current: {current} - Direction: {direction} - Cost: {cost}")
         if distances.get(current) is None:
             distances[current] = cost
 
@@ -153,11 +189,11 @@ def parse_matrix(content: str) -> List[List[str]]:
 
 
 if __name__ == "__main__":
-    with open("input.test.txt", "r") as file:
-        file_content = file.read()
-
-    # with open("input.txt", "r") as file:
+    # with open("input.test.txt", "r") as file:
     #     file_content = file.read()
+
+    with open("input.txt", "r") as file:
+        file_content = file.read()
     print("Part 1")
     start = time.time()
     result = part1(file_content)
@@ -166,6 +202,4 @@ if __name__ == "__main__":
     print("Part 2")
     start = time.time()
     result = part2(file_content)
-    print(
-        f"Result: {result} in {round(time.time() - start, 4)}s; Expected: 74478585072604"
-    )
+    print(f"Result: {result} in {round(time.time() - start, 4)}s; Expected: 498")
